d6724e3cd1ff3b468228ec27bf4e01072ae670bc
Morgane <m.chapeliere@mailistec.fr>
2025-01-06 17:32:20 +0100

    changing backend link

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,16 @@
-import { useEffect, useState } from 'react';
-import { Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { addPlace } from '../reducers/user';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
+import { useEffect, useState } from "react";
+import {
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { addPlace } from "../reducers/user";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function MapScreen() {
   const dispatch = useDispatch();
@@ -12,21 +19,20 @@ export default function MapScreen() {
   const [currentPosition, setCurrentPosition] = useState(null);
   const [tempCoordinates, setTempCoordinates] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
-  const [newPlace, setNewPlace] = useState('');
-  const [savedPlace, setSavedPlace] = useState([])
+  const [newPlace, setNewPlace] = useState("");
+  const [savedPlace, setSavedPlace] = useState([]);
 
-  console.log(savedPlace)
+  console.log(savedPlace);
 
   useEffect(() => {
     (async () => {
       const result = await Location.requestForegroundPermissionsAsync();
       const status = result?.status;
 
-      if (status === 'granted') {
-        Location.watchPositionAsync({ distanceInterval: 10 },
-          (location) => {
-            setCurrentPosition(location.coords);
-          });
+      if (status === "granted") {
+        Location.watchPositionAsync({ distanceInterval: 10 }, (location) => {
+          setCurrentPosition(location.coords);
+        });
       }
     })();
   }, []);
@@ -37,66 +43,100 @@ export default function MapScreen() {
   };
 
   useEffect(() => {
-      fetch(`http://192.168.100.238:3000/places/${user.nickname}`).then(response => response.json()).then(data => {
+    fetch(`https://locapic-backend-omega-eosin.vercel.app${user.nickname}`)
+      .then((response) => response.json())
+      .then((data) => {
         //console.log(data)
-      if(data.result) {
-        //console.log('ok')
-        setSavedPlace( data.places);
-        console.log(savedPlace)
-      }
-    });
-   }, []);
+        if (data.result) {
+          //console.log('ok')
+          setSavedPlace(data.places);
+          console.log(savedPlace);
+        }
+      });
+  }, []);
 
   const handleNewPlace = () => {
-    fetch('http://192.168.100.238:3000/places', {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name: newPlace, latitude: tempCoordinates.latitude, longitude: tempCoordinates.longitude, nickname: user.nickname}),
-}).then(response => response.json())
-.then(data => {
-    //console.log(data)
-    if (data.result) {
-      setModalVisible(false);
-      setNewPlace('');
-    } else {
-      setNewPlace('');
-      console.log('could not addd new place')  
-    }
-})
-};
+    fetch("https://locapic-backend-omega-eosin.vercel.app/places", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: newPlace,
+        latitude: tempCoordinates.latitude,
+        longitude: tempCoordinates.longitude,
+        nickname: user.nickname,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log(data)
+        if (data.result) {
+          setModalVisible(false);
+          setNewPlace("");
+        } else {
+          setNewPlace("");
+          console.log("could not addd new place");
+        }
+      });
+  };
   const handleClose = () => {
     setModalVisible(false);
-    setNewPlace('');
+    setNewPlace("");
   };
-
-
 
   const markers = savedPlace.map((data, i) => {
     //console.log(data)
-    return <Marker key={i} coordinate={{ latitude: data.latitude, longitude: data.longitude }} title={data.name} />;
+    return (
+      <Marker
+        key={i}
+        coordinate={{ latitude: data.latitude, longitude: data.longitude }}
+        title={data.name}
+      />
+    );
   });
 
   //console.log(markers.length);
-  
 
   return (
     <View style={styles.container}>
       <Modal visible={modalVisible} animationType="fade" transparent>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <TextInput placeholder="New place" onChangeText={(value) => setNewPlace(value)} value={newPlace} style={styles.input} />
-            <TouchableOpacity onPress={() => handleNewPlace()} style={styles.button} activeOpacity={0.8}>
+            <TextInput
+              placeholder="New place"
+              onChangeText={(value) => setNewPlace(value)}
+              value={newPlace}
+              style={styles.input}
+            />
+            <TouchableOpacity
+              onPress={() => handleNewPlace()}
+              style={styles.button}
+              activeOpacity={0.8}
+            >
               <Text style={styles.textButton}>Add</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleClose()} style={styles.button} activeOpacity={0.8}>
+            <TouchableOpacity
+              onPress={() => handleClose()}
+              style={styles.button}
+              activeOpacity={0.8}
+            >
               <Text style={styles.textButton}>Close</Text>
             </TouchableOpacity>
           </View>
         </View>
       </Modal>
 
-      <MapView onLongPress={(e) => handleLongPress(e)} mapType="hybrid" style={styles.map}>
-        {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="#fecb2d" />}
+      <MapView
+        onLongPress={(e) => handleLongPress(e)}
+        mapType="hybrid"
+        style={styles.map}
+      >
+        {currentPosition && (
+          <Marker
+            coordinate={currentPosition}
+            title="My position"
+            pinColor="#fecb2d"
+          />
+        )}
         {markers}
       </MapView>
     </View>
@@ -112,15 +152,15 @@ const styles = StyleSheet.create({
   },
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   modalView: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 20,
     padding: 30,
-    alignItems: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -131,22 +171,22 @@ const styles = StyleSheet.create({
   },
   input: {
     width: 150,
-    borderBottomColor: '#ec6e5b',
+    borderBottomColor: "#ec6e5b",
     borderBottomWidth: 1,
     fontSize: 16,
   },
   button: {
     width: 150,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 20,
     paddingTop: 8,
-    backgroundColor: '#ec6e5b',
+    backgroundColor: "#ec6e5b",
     borderRadius: 10,
   },
   textButton: {
-    color: '#ffffff',
+    color: "#ffffff",
     height: 24,
-    fontWeight: '600',
+    fontWeight: "600",
     fontSize: 15,
   },
 });
