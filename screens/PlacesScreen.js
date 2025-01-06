4f670da6345eef14f3b54b531d4bf193a5178486
Morgane <m.chapeliere@mailistec.fr>
2025-01-06 17:32:51 +0100

    changing backend link

--- screens/PlacesScreen.js
+++ screens/PlacesScreen.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState } from "react";
+import { useEffect } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -8,17 +8,17 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-} from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import { useDispatch, useSelector } from 'react-redux';
-import { addPlace, removePlace } from '../reducers/user';
+} from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { useDispatch, useSelector } from "react-redux";
+import { addPlace, removePlace } from "../reducers/user";
 
 export default function PlacesScreen() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.value);
 
-  const [city, setCity] = useState('');
-  const [savedPlace, setSavedPlace] = useState([])
+  const [city, setCity] = useState("");
+  const [savedPlace, setSavedPlace] = useState([]);
 
   const handleSubmit = () => {
     if (city.length === 0) {
@@ -33,41 +33,53 @@ export default function PlacesScreen() {
           latitude: firstCity.geometry.coordinates[1],
           longitude: firstCity.geometry.coordinates[0],
         };
-        fetch('http://192.168.100.238:3000/places', {
+        fetch("https://locapic-backend-omega-eosin.vercel.app/places", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({newPlace}),
-      }).then(response => response.json())
-      .then(data => {
-          //console.log(data)
-          if (data.result) {
-            setSavedPlace([ ...savedPlace, newPlace])
-            setCity('');
-          } 
-      })
-
+          body: JSON.stringify({ newPlace }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            //console.log(data)
+            if (data.result) {
+              setSavedPlace([...savedPlace, newPlace]);
+              setCity("");
+            }
+          });
       });
   };
 
   useEffect(() => {
-    fetch(`http://192.168.100.238:3000/places/${user.nickname}`).then(response => response.json()).then(data => {
-      //console.log(data)
-    if(data.result) {
-      //console.log('ok')
-      setSavedPlace( data.places);
-      console.log(savedPlace)
-    }
-  });
- }, []);
+    fetch(
+      `https://locapic-backend-omega-eosin.vercel.app/places/${user.nickname}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log(data)
+        if (data.result) {
+          //console.log('ok')
+          setSavedPlace(data.places);
+          console.log(savedPlace);
+        }
+      });
+  }, []);
 
   const places = savedPlace.map((data, i) => {
     return (
       <View key={i} style={styles.card}>
         <View>
           <Text style={styles.name}>{data.name}</Text>
-          <Text>LAT : {Number(data.latitude).toFixed(3)} LON : {Number(data.longitude).toFixed(3)}</Text>
+          <Text>
+            LAT : {Number(data.latitude).toFixed(3)} LON :{" "}
+            {Number(data.longitude).toFixed(3)}
+          </Text>
         </View>
-        <FontAwesome name='trash-o' onPress={() => dispatch(removePlace(data.name))} size={25} color='#ec6e5b' />
+        <FontAwesome
+          name="trash-o"
+          onPress={() => dispatch(removePlace(data.name))}
+          size={25}
+          color="#ec6e5b"
+        />
       </View>
     );
   });
@@ -77,8 +89,17 @@ export default function PlacesScreen() {
       <Text style={styles.title}>{user.nickname}'s places</Text>
 
       <View style={styles.inputContainer}>
-        <TextInput placeholder="New city" onChangeText={(value) => setCity(value)} value={city} style={styles.input} />
-        <TouchableOpacity onPress={() => handleSubmit()} style={styles.button} activeOpacity={0.8}>
+        <TextInput
+          placeholder="New city"
+          onChangeText={(value) => setCity(value)}
+          value={city}
+          style={styles.input}
+        />
+        <TouchableOpacity
+          onPress={() => handleSubmit()}
+          style={styles.button}
+          activeOpacity={0.8}
+        >
           <Text style={styles.textButton}>Add</Text>
         </TouchableOpacity>
       </View>
@@ -93,25 +114,25 @@ export default function PlacesScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f2f2f2',
-    alignItems: 'center',
+    backgroundColor: "#f2f2f2",
+    alignItems: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: '600',
+    fontWeight: "600",
     marginTop: 30,
     marginBottom: 20,
   },
   scrollView: {
-    alignItems: 'center',
+    alignItems: "center",
     paddingBottom: 20,
   },
   card: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: '80%',
-    backgroundColor: '#ffffff',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "80%",
+    backgroundColor: "#ffffff",
     padding: 20,
     marginTop: 20,
     borderRadius: 10,
@@ -120,33 +141,33 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '80%',
-    backgroundColor: '#ffffff',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    backgroundColor: "#ffffff",
     padding: 20,
     marginTop: 20,
     borderRadius: 10,
   },
   input: {
-    width: '65%',
+    width: "65%",
     marginTop: 6,
-    borderBottomColor: '#ec6e5b',
+    borderBottomColor: "#ec6e5b",
     borderBottomWidth: 1,
     fontSize: 16,
   },
   button: {
-    width: '30%',
-    alignItems: 'center',
+    width: "30%",
+    alignItems: "center",
     paddingTop: 8,
-    backgroundColor: '#ec6e5b',
+    backgroundColor: "#ec6e5b",
     borderRadius: 10,
   },
   textButton: {
-    color: '#ffffff',
+    color: "#ffffff",
     height: 24,
-    fontWeight: '600',
+    fontWeight: "600",
     fontSize: 15,
   },
 });
